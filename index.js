c3acf3dd30c8a69934039800e5ddfcf989b2ddd7
ROYAL-HarsH <71375205+ROYAL-HarsH@users.noreply.github.com>
2020-12-31 10:33:59 +0530

    hm

--- index.js
+++ index.js
@@ -7,6 +7,35 @@ const config = require(`${process.cwd()}/config.js`);
 
 
 bot.login(process.env.token);
+-require("dotenv").config();//Loading .env
+// Dependencies
+const Discord = require('discord.js');
+const fs = require("fs");
+const { Collection, Client } = require("discord.js");
+
+const client = new Client();//Making a discord bot client
+client.commands = new Collection();//Making client.commands as a Discord.js Collection
+client.queue = new Map()
+
+client.config = {
+  prefix: "?"
+}
+
+//Loading Events
+fs.readdir(__dirname + "/events/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach((file) => {
+    const event = require(__dirname + `/events/${file}`);
+    let eventName = file.split(".")[0];
+    client.on(eventName, event.bind(null, client));
+    console.log("Loading Event: "+eventName)
+  });
+
+// Loads config.js
+const config = require(`${process.cwd()}/config.js`);
+
+
+bot.login(process.env.token);
 
 // Creates shard manager
 const manager = new Discord.ShardingManager(`${process.cwd()}/bot.js`, {
@@ -15,6 +44,16 @@ const manager = new Discord.ShardingManager(`${process.cwd()}/bot.js`, {
     token: config.token
 });
 
+//Loading Commands
+fs.readdir("./commands/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach((file) => {
+    if (!file.endsWith(".js")) return;
+    let props = require(`./commands/${file}`);
+    let commandName = file.split(".")[0];
+    client.commands.set(commandName, props);
+    console.log("Loading Command: "+commandName)
+  });
 /**
  * Client Events
  */
@@ -23,6 +62,8 @@ client.on("ready", () => {
   client.user.setActivity(`?help`, { type: "LISTENING" });
 });
 
+//Logging in to discord
+client.login(process.env.token)
 // Sharding Event handler
 fs.readdir(`${process.cwd()}/events/sharding`, (err, files) => {
     if (err) { throw err }
@@ -37,4 +78,5 @@ fs.readdir(`${process.cwd()}/events/sharding`, (err, files) => {
 
 // Spawns shards
 manager.spawn();
+
 
